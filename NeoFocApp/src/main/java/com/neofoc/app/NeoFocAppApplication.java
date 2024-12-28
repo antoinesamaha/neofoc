79b19986d4e7b229b8f67d3bb9565bec095698b1
Aantoine Samaha <antoine.samaha@01barmaja.com>
2024-12-28 10:47:45 +0200

    Initial neofocApp

---
+++ NeoFocApp/src/main/java/com/neofoc/app/NeoFocAppApplication.java
@@ -0,0 +1,13 @@
+package com.neofoc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoFocAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoFocAppApplication.class, args);
+	}
+
+}
